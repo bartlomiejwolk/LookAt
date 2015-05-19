ad2d2b8e53184345e353604cb7b20178d54a6e31
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-19 14:16:58 +0200

    Cleanup
    - fix error in OnSceneGUI()

--- Editor/LookAtEditor.cs
+++ Editor/LookAtEditor.cs
@@ -1,61 +1,29 @@
 ﻿// Copyright (c) 2015 Bartłomiej Wołk (bartlomiejwolk@gmail.com)
-//  
-// This file is part of the LookAt extension for Unity.
-// Licensed under the MIT license. See LICENSE file in the project root folder.
+// 
+// This file is part of the LookAt extension for Unity. Licensed under the MIT
+// license. See LICENSE file in the project root folder.
 
-using UnityEngine;
-using System.Collections;
 using UnityEditor;
+using UnityEngine;
 
 namespace LookAtEx {
 
-    [CustomEditor(typeof(LookAt))]
+    [CustomEditor(typeof (LookAt))]
     public class LookAtEditor : Editor {
+        #region PROPERTIES
 
-        #region PROPERTIES 
+        private LookAt Script { get; set; }
 
-        LookAt Script { get; set; }
-
-        #endregion
+        #endregion PROPERTIES
 
         #region SERIALIZED PROPERTIES
+
         private SerializedProperty labelStyle;
         private SerializedProperty targetTransform;
-        #endregion
+
+        #endregion SERIALIZED PROPERTIES
 
         #region UNITY MESSAGES
-        public void OnSceneGUI() {
-            LookAt script = (LookAt)target;
-
-            switch (script.Option) {
-                case Options.RotThreshold:
-                    Handles.color = Color.blue;
-                    Handles.Label(
-                        // TODO Add param for label position.
-                            script.transform.position + Vector3.up * 1.5f,
-                            "ThresholdAngle: " + script.ThresholdAngle.ToString(),
-                            script.LabelStyle);
-                    Handles.DrawWireArc(
-                            script.transform.position,
-                            script.transform.up,
-                        // Make the arc simetrical on the left and right
-                        // side of the player.
-                            Quaternion.AngleAxis(
-                                -script.ThresholdAngle / 2,
-                                Vector3.up) * script.transform.forward,
-                            script.ThresholdAngle,
-                            2);
-                    script.ThresholdAngle = Handles.ScaleValueHandle(
-                            script.ThresholdAngle,
-                            script.transform.position + script.transform.forward * 2,
-                            script.transform.rotation,
-                            1,
-                            Handles.ConeCap,
-                            1);
-                    break;
-            }
-        }
-
 
         public void OnEnable() {
             Script = (LookAt) target;
@@ -78,61 +46,40 @@ namespace LookAtEx {
             serializedObject.ApplyModifiedProperties();
         }
 
-        #endregion
-
-        #region INSPECTOR METHODS
-        private void HandleOptionPopup() {
+        // todo refactor
+        public void OnSceneGUI() {
             switch (Script.Option) {
-                case Options.Standard:
-                    break;
-                case Options.YAxisOnly:
-                    break;
-                case Options.RotWithSlerp:
-                    DrawSpeedField();
-                    break;
                 case Options.RotThreshold:
-                    DrawMaxRotSpeedField();
-                    DrawMinTimeToReachField();
-                    DrawThresholdAngleField();
-                    break;
-                case Options.RotWithSDA:
-                    DrawMaxRotSpeedField();
-                    DrawMinTimeToReachField();
+                    Handles.color = Color.blue;
+                    Handles.Label(
+                        // TODO Add param for label position.
+                        Script.transform.position + Vector3.up * 1.5f,
+                        "ThresholdAngle: " + Script.ThresholdAngle,
+                        Script.LabelStyle);
+                    Handles.DrawWireArc(
+                        Script.transform.position,
+                        Script.transform.up,
+                        // Make the arc simetrical on the left and right side
+                        // of the player.
+                        Quaternion.AngleAxis(
+                            -Script.ThresholdAngle / 2,
+                            Vector3.up) * Script.transform.forward,
+                        Script.ThresholdAngle,
+                        2);
+                    Script.ThresholdAngle = Handles.ScaleValueHandle(
+                        Script.ThresholdAngle,
+                        Script.transform.position + Script.transform.forward * 2,
+                        Script.transform.rotation,
+                        1,
+                        Handles.ConeCap,
+                        1);
                     break;
             }
         }
 
-        private void DrawThresholdAngleField() {
-            Script.ThresholdAngle = EditorGUILayout.FloatField(
-                new GUIContent(
-                    "Threshold Angle",
-                    ""),
-                Script.ThresholdAngle);
-        }
+        #endregion UNITY MESSAGES
 
-        private void DrawMinTimeToReachField() {
-            Script.MinTimeToReach = EditorGUILayout.FloatField(
-                new GUIContent(
-                "Min Time to Reach",
-                ""),
-                Script.MinTimeToReach);
-        }
-
-        private void DrawMaxRotSpeedField() {
-            Script.MaxRotSpeed = EditorGUILayout.FloatField(
-                new GUIContent(
-                "Max Rot. Speed",
-                ""),
-                Script.MaxRotSpeed);
-        }
-
-        private void DrawSpeedField() {
-            Script.Speed = EditorGUILayout.FloatField(
-                new GUIContent(
-                "Speed",
-                    ""),
-                Script.Speed);
-        }
+        #region INSPECTOR METHODS
 
         private void DrawInstantRotationToggle() {
             Script.ClickInstantRot = EditorGUILayout.Toggle(
@@ -140,24 +87,6 @@ namespace LookAtEx {
                     "Instant Rotation",
                     ""),
                 Script.ClickInstantRot);
-        }
-
-        private void DrawOverrideRootField() {
-            Script.OverrideRoot = (Transform) EditorGUILayout.ObjectField(
-                new GUIContent(
-                    "Transform",
-                    ""), 
-                Script.OverrideRoot,
-                typeof (Transform),
-                true);
-        }
-
-        private void DrawOptionPopup() {
-            Script.Option = (Options) EditorGUILayout.EnumPopup(
-                new GUIContent(
-                    "Option",
-                    ""),
-                Script.Option);
         }
 
         private void DrawLabelStyleControls() {
@@ -169,6 +98,48 @@ namespace LookAtEx {
                 true);
         }
 
+        private void DrawMaxRotSpeedField() {
+            Script.MaxRotSpeed = EditorGUILayout.FloatField(
+                new GUIContent(
+                    "Max Rot. Speed",
+                    ""),
+                Script.MaxRotSpeed);
+        }
+
+        private void DrawMinTimeToReachField() {
+            Script.MinTimeToReach = EditorGUILayout.FloatField(
+                new GUIContent(
+                    "Min Time to Reach",
+                    ""),
+                Script.MinTimeToReach);
+        }
+
+        private void DrawOptionPopup() {
+            Script.Option = (Options) EditorGUILayout.EnumPopup(
+                new GUIContent(
+                    "Option",
+                    ""),
+                Script.Option);
+        }
+
+        private void DrawOverrideRootField() {
+            Script.OverrideRoot = (Transform) EditorGUILayout.ObjectField(
+                new GUIContent(
+                    "Transform",
+                    ""),
+                Script.OverrideRoot,
+                typeof (Transform),
+                true);
+        }
+
+        private void DrawSpeedField() {
+            Script.Speed = EditorGUILayout.FloatField(
+                new GUIContent(
+                    "Speed",
+                    ""),
+                Script.Speed);
+        }
+
         private void DrawTargetTransformField() {
             EditorGUILayout.PropertyField(
                 targetTransform,
@@ -176,16 +147,52 @@ namespace LookAtEx {
                     "Target Transform",
                     ""));
         }
-        #endregion
+
+        private void DrawThresholdAngleField() {
+            Script.ThresholdAngle = EditorGUILayout.FloatField(
+                new GUIContent(
+                    "Threshold Angle",
+                    ""),
+                Script.ThresholdAngle);
+        }
+
+        private void HandleOptionPopup() {
+            switch (Script.Option) {
+                case Options.Standard:
+                    break;
+
+                case Options.YAxisOnly:
+                    break;
+
+                case Options.RotWithSlerp:
+                    DrawSpeedField();
+                    break;
+
+                case Options.RotThreshold:
+                    DrawMaxRotSpeedField();
+                    DrawMinTimeToReachField();
+                    DrawThresholdAngleField();
+                    break;
+
+                case Options.RotWithSDA:
+                    DrawMaxRotSpeedField();
+                    DrawMinTimeToReachField();
+                    break;
+            }
+        }
+
+        #endregion INSPECTOR METHODS
+
         #region METHODS
 
         [MenuItem("Component/LookAt")]
         private static void AddLookAtComponent() {
             if (Selection.activeGameObject != null) {
-                Selection.activeGameObject.AddComponent(typeof(LookAt));
+                Selection.activeGameObject.AddComponent(typeof (LookAt));
             }
         }
 
         #endregion METHODS
     }
+
 }
--- LookAt.cs
+++ LookAt.cs
@@ -1,14 +1,14 @@
 ﻿// Copyright (c) 2015 Bartłomiej Wołk (bartlomiejwolk@gmail.com)
-//  
-// This file is part of the LookAt extension for Unity.
-// Licensed under the MIT license. See LICENSE file in the project root folder.
+// 
+// This file is part of the LookAt extension for Unity. Licensed under the MIT
+// license. See LICENSE file in the project root folder.
 
 using UnityEngine;
-using System.Collections;
 
 namespace LookAtEx {
+
     /// Just like Unity's LookAt but with more options.
-    ///
+    /// 
     /// Options available:
     /// - Standard
     /// - YAxisOnly
@@ -16,103 +16,106 @@ namespace LookAtEx {
     /// - RotThreshold
     /// - RotWithSDA
     public class LookAt : MonoBehaviour {
-
-        #region FIELDS
-        #endregion
-
         #region INSPECTOR FIELDS
-        /// Variable required by SmoothDampAngle().
-        // todo encapsulate
-        private float velocity;
 
-        /// Transform used to calculate angle between himself 
-        /// and the target.
-        ///
-        /// Use it if you need sth. else that root transform.
-        [SerializeField]
-        private Transform overrideRoot;
-
-        /// When this is enabled, on mouse click a full rotation
-        /// is applied.
+        /// When this is enabled, on mouse click a full rotation is applied.
         [SerializeField]
         private bool clickInstantRot;
-
-        /// Minimum time to reach target.
-        [SerializeField]
-        private float minTimeToReach;
-
-        /// Maximum rotation speed.
-        [SerializeField]
-        private float maxRotSpeed;
-
-        /// Rotation speed.
-        [SerializeField]
-        private float speed;
-
-        /// Dead zone angle where rotation don't accours.
-        [SerializeField]
-        private float thresholdAngle;
-
-        /// Object to look at.
-        [SerializeField]
-        private Transform targetTransform;
-
-        [SerializeField]
-        // How LookAt controller should work.
-        private Options option;
 
         /// GUIStyle options for scene view labels.
         [SerializeField]
         private GUIStyle labelStyle;
 
-        #endregion
+        /// Maximum rotation speed.
+        [SerializeField]
+        private float maxRotSpeed;
+
+        /// Minimum time to reach target.
+        [SerializeField]
+        private float minTimeToReach;
+
+        [SerializeField]
+        // How LookAt controller should work.
+        private Options option;
+
+        /// Transform used to calculate angle between himself and the target.
+        /// 
+        /// Use it if you need sth. else that root transform.
+        [SerializeField]
+        private Transform overrideRoot;
+
+        /// Rotation speed.
+        [SerializeField]
+        private float speed;
+
+        /// Object to look at.
+        [SerializeField]
+        private Transform targetTransform;
+
+        /// Dead zone angle where rotation don't accours.
+        [SerializeField]
+        private float thresholdAngle;
+
+        /// Variable required by SmoothDampAngle().
+        // todo encapsulate
+        private float velocity;
+
+        #endregion INSPECTOR FIELDS
 
         #region PROPERTIES
-        public Options Option {
-            get { return option; }
-            set { option = value; }
-        }
 
-        // TODO Add public Vector3 field for selecting
-        // axis to lock.
-        public Transform TargetTransform {
-            get { return targetTransform; }
-            set { targetTransform = value; }
-        }
-        public float ThresholdAngle {
-            get { return thresholdAngle; }
-            set { thresholdAngle = value; }
-        }
-        public float Speed {
-            get { return speed; }
-            set { speed = value; }
-        }
-        public float MaxRotSpeed {
-            get { return maxRotSpeed; }
-            set { maxRotSpeed = value; }
-        }
-        public float MinTimeToReach {
-            get { return minTimeToReach; }
-            set { minTimeToReach = value; }
-        }
         public bool ClickInstantRot {
             get { return clickInstantRot; }
             set { clickInstantRot = value; }
         }
-        public Transform OverrideRoot {
-            get { return overrideRoot; }
-            set { overrideRoot = value; }
-        }
+
         public GUIStyle LabelStyle {
             get { return labelStyle; }
             set { labelStyle = value; }
         }
 
+        public float MaxRotSpeed {
+            get { return maxRotSpeed; }
+            set { maxRotSpeed = value; }
+        }
+
+        public float MinTimeToReach {
+            get { return minTimeToReach; }
+            set { minTimeToReach = value; }
+        }
+
+        public Options Option {
+            get { return option; }
+            set { option = value; }
+        }
+
+        public Transform OverrideRoot {
+            get { return overrideRoot; }
+            set { overrideRoot = value; }
+        }
+
+        public float Speed {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        // TODO Add public Vector3 field for selecting axis to lock.
+        public Transform TargetTransform {
+            get { return targetTransform; }
+            set { targetTransform = value; }
+        }
+
+        public float ThresholdAngle {
+            get { return thresholdAngle; }
+            set { thresholdAngle = value; }
+        }
+
         private Transform MyTransform { get; set; }
 
-        #endregion
+        #endregion PROPERTIES
 
         #region UNITY MESSAGES
+
         private void Awake() {
             if (!targetTransform) {
                 // todo Add Utilities class
@@ -120,11 +123,11 @@ namespace LookAtEx {
             }
         }
 
-        private void Start () {
+        private void Start() {
             MyTransform = GetComponent<Transform>();
         }
-        
-        private void Update () {
+
+        private void Update() {
             if (!targetTransform) {
                 return;
             }
@@ -133,42 +136,106 @@ namespace LookAtEx {
                 case Options.Standard:
                     MyTransform.LookAt(targetTransform);
                     break;
+
                 case Options.YAxisOnly:
-                    Vector3 v = targetTransform.position - MyTransform.position;
+                    var v = targetTransform.position - MyTransform.position;
                     v.x = v.z = 0.0f;
-                    MyTransform.LookAt(targetTransform.transform.position - v); 
+                    MyTransform.LookAt(targetTransform.transform.position - v);
                     break;
+
                 case Options.RotWithSlerp:
                     RotateWithSlerp();
                     break;
+
                 case Options.RotThreshold:
                     RotateWithinThreshold();
                     break;
+
                 case Options.RotWithSDA:
                     RotateWithSDA();
                     break;
             }
         }
-        #endregion
+
+        #endregion UNITY MESSAGES
 
         #region METHODS
-        private void RotateWithSlerp() {
-            // Rotation to be applied.
-            Vector3 newRotation;
-            // Direction to the target.
-            Vector3 dir;
 
-            dir = targetTransform.position - MyTransform.position;
-            // Calculate rotation to the target.
-            newRotation = Quaternion.LookRotation(dir).eulerAngles;
-            // Rotate only around y axis.
-            newRotation.x = 0;
-            newRotation.z = 0;
-            // Apply rotation.
-            MyTransform.rotation = Quaternion.Slerp(
-                    MyTransform.rotation,
-                    Quaternion.Euler(newRotation),
-                    Time.deltaTime * speed);
+        /// The angle between dirA and dirB around axis
+        // TODO Create static class TransformTools and move it there.
+        public static float AngleAroundAxis(
+            Vector3 dirA,
+            Vector3 dirB,
+            Vector3 axis) {
+            // Project A and B onto the plane orthogonal _target axis
+            dirA = dirA - Vector3.Project(dirA, axis);
+            dirB = dirB - Vector3.Project(dirB, axis);
+
+            // Find (positive) angle between A and B
+            var angle = Vector3.Angle(dirA, dirB);
+
+            // Return angle multiplied with 1 or -1
+            return angle
+                   * (Vector3.Dot(axis, Vector3.Cross(dirA, dirB)) < 0 ? -1 : 1);
+        }
+
+        private void RotateWithinThreshold() {
+            // Angle between character forword vector and the target.
+            float hAngle;
+            // Angle between custom selected transform and the target. You can
+            // select custom transtorm in the inspector.
+            float hAngleCustom;
+            // How much the target is beyond the specified threshold (in
+            // degrees).
+            float hAngleThr;
+            // Rotation that will be applied to the transform. When rotation is
+            // finished, should always be 0.
+            float newRotation = 0;
+
+            hAngle = AngleAroundAxis(
+                MyTransform.forward,
+                targetTransform.position - MyTransform.position,
+                Vector3.up);
+
+            if (overrideRoot) {
+                hAngleCustom = AngleAroundAxis(
+                    overrideRoot.forward,
+                    targetTransform.position - overrideRoot.position,
+                    Vector3.up);
+            }
+            else {
+                hAngleCustom = AngleAroundAxis(
+                    MyTransform.forward,
+                    targetTransform.position - MyTransform.position,
+                    Vector3.up);
+            }
+
+            // On click, omit all the smoothing code and rotate transform
+            // immediately.
+            if (Input.GetKey(KeyCode.Mouse0) && clickInstantRot) {
+                MyTransform.Rotate(0, hAngleCustom, 0);
+                return;
+            }
+
+            // If target is within specified threshold, return 0 which means,
+            // that there's no reason to rotate.
+            hAngleThr = Mathf.Max(
+                0,
+                Mathf.Abs(hAngle) - thresholdAngle);
+            // If target is beyond specified threshold, add a sign to know
+            // which direction to rotate.
+            hAngleThr *= Mathf.Sign(hAngle);
+
+            // Rotation to be applied.
+            newRotation = Mathf.SmoothDampAngle(
+                newRotation,
+                hAngleThr,
+                ref velocity,
+                minTimeToReach,
+                maxRotSpeed);
+
+            // Apply rotation to the transform.
+            MyTransform.Rotate(0, newRotation, 0);
         }
 
         private void RotateWithSDA() {
@@ -190,108 +257,36 @@ namespace LookAtEx {
             angles.z = 0;
             // Apply rotation.
             MyTransform.rotation = Quaternion.Euler(
-                    angles.x,
-                    Mathf.SmoothDampAngle(
-                        angles.y,
-                        newRotation.y,
-                        ref velocity,
-                        minTimeToReach,
-                        maxRotSpeed),
-                    angles.z);
-        }
-
-        private void RotateWithinThreshold() {
-            // Angle between character forword vector and the target.
-            float hAngle;
-            // Angle between custom selected transform and the target.
-            // You can select custom transtorm in the inspector.
-            float hAngleCustom;
-            // How much the target is beyond the specified threshold
-            // (in degrees).
-            float hAngleThr;
-            // Rotation that will be applied to the transform.
-            // When rotation is finished, should always be 0.
-            float newRotation = 0;
-
-
-            hAngle = AngleAroundAxis(
-                    MyTransform.forward,
-                    targetTransform.position - MyTransform.position,
-                    Vector3.up);
-
-            if (overrideRoot) {
-                hAngleCustom = AngleAroundAxis(
-                        overrideRoot.forward,
-                        targetTransform.position - overrideRoot.position,
-                        Vector3.up);
-            }
-            else {
-                hAngleCustom = AngleAroundAxis(
-                        MyTransform.forward,
-                        targetTransform.position - MyTransform.position,
-                        Vector3.up);
-            }
-
-            // On click, omit all the smoothing code and rotate
-            // transform immediately.
-            if (Input.GetKey(KeyCode.Mouse0) && clickInstantRot) {
-                MyTransform.Rotate(0, hAngleCustom, 0);
-                return;
-            }
-
-            // If target is within specified threshold, return 0
-            // which means, that there's no reason to rotate.
-            hAngleThr = Mathf.Max(
-                    0,
-                    Mathf.Abs(hAngle) - thresholdAngle);
-            // If target is beyond specified threshold, add a sign
-            // to know which direction to rotate.
-            hAngleThr *= Mathf.Sign(hAngle);
-
-            // Rotation to be applied.
-            newRotation = Mathf.SmoothDampAngle(
-                    newRotation,
-                    hAngleThr,
+                angles.x,
+                Mathf.SmoothDampAngle(
+                    angles.y,
+                    newRotation.y,
                     ref velocity,
                     minTimeToReach,
-                    maxRotSpeed);
-
-            // Apply rotation to the transform.
-            MyTransform.Rotate (0, newRotation, 0);
-
-            // TODO Remove debug lines.
-            /*Debug.DrawLine(
-                    _overrideRoot.position,
-                    _overrideRoot.position + _overrideRoot.forward * 20,
-                    Color.green);*/
-            /*Debug.DrawLine(
-                    _transform.position,
-                    _transform.position + _transform.forward * 20,
-                    Color.yellow);
-            MeasureIt_ODG.Set("_transform.rotation", _transform.rotation.eulerAngles);
-            MeasureIt_ODG.Set("Cursor abs angle", AngleAroundAxis(
-                        Vector3.forward,
-                        _target.position,
-                        Vector3.up));
-            MeasureIt_ODG.Set("hAngle", hAngle);
-            MeasureIt_ODG.Set("hAngleThr", hAngleThr);
-            MeasureIt_ODG.Set("AngleAxis", Quaternion.AngleAxis(hAngleThr, Vector3.up).eulerAngles);*/
+                    maxRotSpeed),
+                angles.z);
         }
-     
-        /// The angle between dirA and dirB around axis
-        // TODO Create static class TransformTools and move it there.
-        public static float AngleAroundAxis (Vector3 dirA, Vector3 dirB, Vector3 axis) {
-            // Project A and B onto the plane orthogonal _target axis
-            dirA = dirA - Vector3.Project(dirA, axis);
-            dirB = dirB - Vector3.Project(dirB, axis);
-     
-            // Find (positive) angle between A and B
-            float angle = Vector3.Angle(dirA, dirB);
-     
-            // Return angle multiplied with 1 or -1
-            return angle * (Vector3.Dot(axis, Vector3.Cross(dirA, dirB)) < 0 ? -1 : 1);
+
+        private void RotateWithSlerp() {
+            // Rotation to be applied.
+            Vector3 newRotation;
+            // Direction to the target.
+            Vector3 dir;
+
+            dir = targetTransform.position - MyTransform.position;
+            // Calculate rotation to the target.
+            newRotation = Quaternion.LookRotation(dir).eulerAngles;
+            // Rotate only around y axis.
+            newRotation.x = 0;
+            newRotation.z = 0;
+            // Apply rotation.
+            MyTransform.rotation = Quaternion.Slerp(
+                MyTransform.rotation,
+                Quaternion.Euler(newRotation),
+                Time.deltaTime * speed);
         }
-        #endregion
+
+        #endregion METHODS
     }
 
 }
